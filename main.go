51555c6b32765b4b9b616a4d68bc55541019eac9
zico <izallyjr@gmail.com>
2026-01-09 03:07:57 -0500

    feature: add cleanbody function to remove bad words

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
@@ -19,10 +21,10 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.Header().Add("Content-Type", "text/html; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	hits := cfg.fileserverHits.Load()
-	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+	w.Write([]byte(fmt.Sprintf("<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", hits)))
 }
 
 func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
@@ -36,9 +38,10 @@ func main() {
 	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("POST /reset", apiCfg.resetMetrics)
-	mux.HandleFunc("GET /metrics", apiCfg.getMetrics)
-	mux.HandleFunc("GET /healthz", handleHealth)
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
+	mux.HandleFunc("POST /admin/reset", apiCfg.resetMetrics)
+	mux.HandleFunc("GET /admin/metrics", apiCfg.getMetrics)
+	mux.HandleFunc("GET /api/healthz", handleHealth)
 	s := &http.Server{
 		Addr:    ":8080",
 		Handler: middlewareLog(mux),
@@ -58,4 +61,76 @@ func middlewareLog(next http.Handler) http.Handler {
 		log.Printf("%s %s", r.Method, r.URL.Path)
 		next.ServeHTTP(w, r)
 	})
+}
+
+func handleValidateChirp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	type params struct {
+		Body string `json:"body"`
+	}
+	type errResBody struct {
+		Error string `json:"error"`
+	}
+	type returnVal struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	reqBody := params{}
+
+	err := decoder.Decode(&reqBody)
+	if err != nil {
+		log.Printf("Error parsing request body: %s\n", err)
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	if len(reqBody.Body) > 140 {
+		log.Printf("Chirp was too long: %d characters\n", len(reqBody.Body))
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	blockedWords := map[string]struct{}{
+		"herfuffle": {},
+		"sharbert":  {},
+		"fornax":    {},
+	}
+	text := cleanBody(reqBody.Body, blockedWords)
+
+	respondWithJSON(w, 200, returnVal{CleanedBody: text})
+}
+
+func cleanBody(body string, blockedWords map[string]struct{}) string {
+	words := strings.Split(body, " ")
+	for i, word := range words {
+		if _, ok := blockedWords[strings.ToLower(word)]; ok {
+			words[i] = "****"
+		}
+	}
+
+	return strings.Join(words, " ")
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errResBody struct {
+		Error string `json:"error"`
+	}
+
+	w.WriteHeader(code)
+	data, err := json.Marshal(errResBody{Error: msg})
+	if err == nil {
+		w.Write(data)
+	}
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error creating response body: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+	w.WriteHeader(code)
+	w.Write(data)
 }
