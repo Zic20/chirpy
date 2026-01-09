15bcd6654319a0a57ad39e4fb06eb62e731401a4
zico <izallyjr@gmail.com>
2026-01-09 01:14:26 -0500

    feature: added pattern matching to endpoints

--- main.go
+++ main.go
@@ -1,21 +1,47 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-type apiHandler struct{}
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
-func (apiHandler) ServeHTTP(http.ResponseWriter, *http.Request) {}
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) getMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	w.Write([]byte(fmt.Sprintf("Hits: %d", hits)))
+}
+
+func (cfg *apiConfig) resetMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
 
 func main() {
+	apiCfg := apiConfig{}
 	mux := http.NewServeMux()
-	fs := http.FileServer(http.Dir("."))
-	mux.Handle("/app/", http.StripPrefix("/app", fs))
-	mux.HandleFunc("/healthz", handleHealth)
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("POST /reset", apiCfg.resetMetrics)
+	mux.HandleFunc("GET /metrics", apiCfg.getMetrics)
+	mux.HandleFunc("GET /healthz", handleHealth)
 	s := &http.Server{
 		Addr:    ":8080",
-		Handler: mux,
+		Handler: middlewareLog(mux),
 	}
 
 	s.ListenAndServe()
@@ -25,4 +51,11 @@ func handleHealth(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
 }
